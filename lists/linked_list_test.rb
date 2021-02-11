1d3de253dcbb35c89c702fd5682c9ff115fb1780
enjaku <enjaku4@gmail.com>
2021-02-11 19:30:08 +0200

    proper assertion usage

--- lists/linked_list_test.rb
+++ lists/linked_list_test.rb
@@ -18,30 +18,30 @@ class LinkedListTest < Minitest::Test
 
   def test_appending_x_changes_tail_to_x
     @list.append(42)
-    assert_equal(@list.tail.value, 42)
+    assert_equal(42, @list.tail.value)
 
     @list.append(398)
-    assert_equal(@list.tail.value, 398)
+    assert_equal(398, @list.tail.value)
   end
 
   def test_appending_does_not_change_existing_head
     @list.append(42)
     @list.append(8)
-    assert_equal(@list.head.value, 42)
+    assert_equal(42, @list.head.value)
   end
 
   def test_prepending_x_changes_head_to_x
     @list.prepend(42)
-    assert_equal(@list.head.value, 42)
+    assert_equal(42, @list.head.value)
 
     @list.prepend(398)
-    assert_equal(@list.head.value, 398)
+    assert_equal(398, @list.head.value)
   end
 
   def test_prepending_does_not_change_existing_tail
     @list.prepend(42)
     @list.prepend(8)
-    assert_equal(@list.tail.value, 42)
+    assert_equal(42, @list.tail.value)
   end
 
   def test_searching_for_nonexistent_previous_node_returns_nil
@@ -58,7 +58,7 @@ class LinkedListTest < Minitest::Test
     @list.append(5)
     @list.append(59)
     assert_kind_of(LinkedList::Node, @list.find_before(59))
-    assert_equal(@list.find_before(59).value, 5)
+    assert_equal(5, @list.find_before(59).value)
   end
 
   def test_searching_for_nonexistent_node_returns_nil
@@ -72,7 +72,7 @@ class LinkedListTest < Minitest::Test
     @list.append(5)
     @list.append(59)
     assert_kind_of(LinkedList::Node, @list.find(59))
-    assert_equal(@list.find(59).value, 59)
+    assert_equal(59, @list.find(59).value)
   end
 
   def test_inserting_after_nonexistent_node_does_nothing
@@ -87,15 +87,15 @@ class LinkedListTest < Minitest::Test
   def test_inserts_new_node_after_tail
     @list.append(68)
     @list.insert(3, after: 68)
-    assert_equal(@list.find_before(3).value, 68)
+    assert_equal(68, @list.find_before(3).value)
   end
 
   def test_inserts_new_node_in_between
     @list.append(68)
     @list.append(139)
     @list.insert(33, after: 68)
-    assert_equal(@list.find_before(33).value, 68)
-    assert_equal(@list.find_before(139).value, 33)
+    assert_equal(68, @list.find_before(33).value)
+    assert_equal(33, @list.find_before(139).value)
   end
 
   def test_deleting_nonexistent_node_does_nothing
@@ -104,7 +104,7 @@ class LinkedListTest < Minitest::Test
 
     @list.append(68)
     @list.delete(3)
-    assert_equal(@list.head.value, 68)
+    assert_equal(68, @list.head.value)
   end
 
   def test_deletes_the_only_node
@@ -117,7 +117,7 @@ class LinkedListTest < Minitest::Test
     @list.append(68)
     @list.append(5)
     @list.delete(68)
-    assert_equal(@list.head.value, 5)
+    assert_equal(5, @list.head.value)
   end
 
   def test_deletes_node_in_between
@@ -125,12 +125,12 @@ class LinkedListTest < Minitest::Test
     @list.append(23)
     @list.append(5)
     @list.delete(23)
-    assert_equal(@list.find_before(5).value, 9)
+    assert_equal(9, @list.find_before(5).value)
   end
 
   def test_deletes_tail
     @list.append(9)
     @list.delete(5)
-    assert_equal(@list.tail.value, 9)
+    assert_equal(9, @list.tail.value)
   end
 end
